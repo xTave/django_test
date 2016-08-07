5730754683e9fcc39f84d2bab82c4304bed2dacf
xTave <MishaLevshin@list.ru>
2016-08-07 23:08:43 +0500

    12312

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
 
 def main_page(request):
     return render(request, 'blog/main_page.html', {})
 
+
 def blog_page(request):
-    return render(request, 'blog/blog_page.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/blog_page.html', {"posts": posts})
