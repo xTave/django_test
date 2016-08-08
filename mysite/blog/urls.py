3fa8143f2d3237264ff2b72acf6bc25fbcd36430
xTave <MishaLevshin@list.ru>
2016-08-08 11:38:34 +0500

    41241

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
-    url(r'^blog$', views.blog_page, name="blog_page")
+    url(r'^blog$', views.blog_page, name="blog_page"),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/delete/$', views.post_delete, name="post_delete")
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 
 def main_page(request):
@@ -8,5 +9,46 @@ def main_page(request):
 
 
 def blog_page(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/blog_page.html', {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = [get_object_or_404(Post, pk=pk)]
+    return render(request, "blog/blog_page.html", {"posts": post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form, 'edit': False})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form, 'edit': True})
+
+
+def post_delete(request, pk):
+    print("1234" + str(pk))
+    post = Post.objects.filter(pk=pk)
+    post.delete()
+    return redirect("blog.views.blog_page")
